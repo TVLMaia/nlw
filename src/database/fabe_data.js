d9ac368902a873106dbd6bc0f81eef2978e20622
TVLMaia <56745146+TVLMaia@users.noreply.github.com>
2020-08-09 00:28:30 -0300

    the database has ended

---
+++ src/database/fabe_data.js
@@ -0,0 +1,36 @@
+//dados
+const proffys = [
+    {
+        name:"Diego Fernandes",
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp:"11932985624",
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost:"20",
+        weekday:[0],
+        time_from:[720],
+        time_to:[1228]
+    },
+    {
+            name:"Luiza Ribeiro",
+             avatar:"https://media-exp1.licdn.com/dms/image/C4D03AQEtd42QiT2svg/profile-displayphoto-shrink_100_100/0?e=1602115200&v=beta&t=taxax648YqmcHgtqU7J3ChY_nN_p7rNUHK6eCEcqJjE",
+             whatsapp:"11932985624", 
+             bio:"Entusiasta das melhores tecnologias de química avançada.<br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+             subject:"Química",
+             cost:"20",
+             weekday:[1],
+             time_from:[720],
+             time_to:[1228]
+    },
+    {
+        name:"Luiza Ribeiro",
+        avatar:"https://media-exp1.licdn.com/dms/image/C4D03AQEtd42QiT2svg/profile-displayphoto-shrink_100_100/0?e=1602115200&v=beta&t=taxax648YqmcHgtqU7J3ChY_nN_p7rNUHK6eCEcqJjE", 
+        whatsapp:"11932985624", 
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject:"Química",
+        cost:"20",
+        weekday:[1],
+        time_from:[720],
+        time_to:[1228]
+}
+]
